ba5f669c9f677b163e9c9780c42f0f4038428120
matthewmain <matthewwmain@gmail.com>
2015-09-14 15:40:20 -0400

    wrote "deli counter" program

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+katz_deli = []
+
+def take_a_number(name_arr, new_name)
+  name_arr.push(new_name)
+   puts "Welcome, #{new_name}. You are number #{name_arr.index(new_name)+1} in line."
+  name_arr.index(new_name)+1
+end
+
+def now_serving(name_arr)
+	if name_arr[0] == nil
+		puts "There is nobody waiting to be served!" 
+	else
+		puts "Currently serving #{name_arr[0]}."
+		name_arr.shift
+	end
+end
+
+def line(name_arr)
+	if name_arr[0] == nil
+		puts "The line is currently empty."
+	else
+        		message = "The line is currently:"
+       		name_arr.each {|i| message << " #{name_arr.index(i)+1}. #{i}"}
+        		puts message
+	end
+end
