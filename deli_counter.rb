50a3772cc3cfe09a1035942afecc9719e7ef213c
skipsuva <skip.suva@gmail.com>
2015-09-15 07:22:10 -0400

    Built a functioning Deli Counter

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+
+def line(line)
+  if line.empty? == true
+    puts "The line is currently empty."
+  else
+    print_line(line)
+  end
+end
+
+def print_line(line)
+  line_text = "The line is currently:"
+  line.each_with_index do |name, index|
+    line_text << " #{index+1}. #{name}"
+  end
+  puts line_text
+end
+
+def take_a_number(line, name)
+  line.push(name)
+  puts "Welcome, #{name}. You are number #{line.size} in line."
+end
+
+def now_serving(line)
+  line.empty? ? puts("There is nobody waiting to be served!") : puts("Currently serving #{line.shift}.")
+end
+
+
+
+
