2e86fb4face89128ece3c1af17b93e01e35c956a
morgvanny <morgvanny@gmail.com>
2015-10-25 04:48:50 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.empty?
+    puts "The line is currently empty."
+  else
+    status = Array.new
+    katz_deli.each do |name|
+      status << "#{katz_deli.index(name)+1}. #{name}"
+    end
+    puts "The line is currently: #{status}"
+  end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+end
