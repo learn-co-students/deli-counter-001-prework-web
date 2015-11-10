fbcff6b6db2958d15e9b18bfbb0207096c68c23b
wmmcbride <w.matt.mcbride@gmail.com>
2015-11-10 03:15:54 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,3 @@
-# Write your code here.
-
 katz_deli = []  #create an empty array
 
 def take_a_number(katz_deli, name)
@@ -7,6 +5,7 @@ def take_a_number(katz_deli, name)
   position = katz_deli.index(name) + 1 #assign the position to the new array item but add 1 because we do not want to start at zero
   puts "Welcome, #{name}. You are number #{position} in line."  #output the message accordingly inserting the variable strings announcing the name of the person and their position in the array
 end
+
 
 def now_serving(katz_deli)
   if katz_deli.size > 0  #if the array size is greater than zero, then the method needs to output a phrase with that person's name in the first position of the array (i.e. index zero)
@@ -17,13 +16,16 @@ def now_serving(katz_deli)
   end
 end
 
+
 def line(katz_deli)
   if katz_deli.size == 0 #if the array size is zero then it needs to output the phrase below
     puts "The line is currently empty."  #outputting the appropriate phrase if the array size is zero
   else
     line_list = "The line is currently:" #if the array size is not zero (i.e. greater than zero), needs to output list of customers starting with this phrase
 
-    #need to identify the name and index position. Using each_with_index because needs to pull both of these items. Block assigns the array items to be pulls in the enumeration. Need to add       each array item to the string line_list starting with the index [position + 1] so it starts at one rather than zero, then a period, then the name
+    #Need to identify the name and index position. Using each_with_index [referenced via Ruby Doc at <http://ruby-doc.org/core-2.2.3/Enumerable.html>]
+    #because needs to pull both of these items. Block assigns the array items to be pulls in the enumeration.
+    #Need to add each array item to the string line_list starting with the index [position + 1] so it starts at one rather than zero, then a period, then the name.
 
     katz_deli.each_with_index {|name, i| line_list << " #{i+1}. #{name}"}
     puts line_list #outputs the string with the array positions and names added to the string in sequence
