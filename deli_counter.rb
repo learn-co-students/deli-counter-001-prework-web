1657e1ab6cb5e1b4e16b25ce16d0e18813261b30
dunxtand <mdrstandish@gmail.com>
2015-11-17 14:44:29 -0500

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,16 +1,16 @@
 def take_a_number(line_array, name)
-  line_array << name
-  spot = line_array.index(name)+1
-  puts "Welcome, #{name}. You are number #{spot} in line."
-  spot
+  puts "Welcome, #{name}. You are number #{line_array.push(name).length} in line."
 end
 
 def line(line_array)
   if line_array.empty?
     puts "The line is currently empty."
   else
-    line_string = ""
-    line_array.each { |name| line_string += (" #{line_array.index(name)+1}. " + name) }
+    line_string, count = "", 0
+    line_array.each do |name|
+      count += 1
+      line_string += " #{count}. #{name}"
+    end
     puts "The line is currently:#{line_string}"
   end
 end
@@ -19,7 +19,6 @@ def now_serving(line_array)
   if line_array.empty?
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving " + line_array[0] + "."
-    line_array.delete_at(0)
+    puts "Currently serving #{line_array.shift}."
   end
 end
