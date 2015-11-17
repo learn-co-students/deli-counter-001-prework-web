094892fb42d1afad476d639aa46058045467a164
RodAquino <rodrigo_59@hotmail.com>
2015-11-17 13:07:29 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,48 @@
 # Write your code here.
+katz_deli = []
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  position = katz_deli.index(name)
+  puts"Welcome, #{name}. You are number #{katz_deli.index(name)+1} in line."
+
+  return name, position
+end
+
+  def line(katz_deli)
+ if katz_deli.empty? == false
+
+  str = "The line is currently:"
+katz_deli.each_with_index do |name, index|
+   str += " #{index + 1}. #{name}"
+
+
+end
+puts str
+#puts "The line is currently:"
+   #katz_deli.each_with_index do|name,index|
+    # print"The line is currently: #{index+1}. #{name}"
+# end
+
+     elsif katz_deli.empty? == true
+  puts "The line is currently empty."
+ end
+end
+
+
+
+
+def now_serving (katz_deli)
+  if katz_deli.empty? == true
+    puts"There is nobody waiting to be served!"
+  elsif katz_deli.empty? == false
+
+    puts"Currently serving #{katz_deli.first}."
+  katz_deli.shift
+  end
+end
+
+
+#name = "Morgana"
+#line(["Logan", "Avi", "Spencer"])
+#take_a_number(katz_deli,name)
