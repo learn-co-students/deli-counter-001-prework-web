2b668f388fe3f71feaf1a38523877c37853414e7
shmulim <shmuel.markel@gmail.com>
2015-11-19 00:39:08 -0800

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+def line(line)
+  if line.size == 0
+    puts "The line is currently empty."
+  else
+  lineup = String.new
+  line.each.with_index { |name, position| lineup += " #{position +=1}. #{name}" }
+  puts "The line is currently:" + lineup
+  end
+end
+
+def take_a_number(line, name)
+  line << name
+  puts "Welcome, #{name}. You are number #{line.size} in line."
+end
+
+def now_serving
+
+end
