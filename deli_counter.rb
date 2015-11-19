91027e47ad4953f9c69d28d580e2048754ccc9d4
ETphonehomeslice <et88guy@hotmail.com>
2015-11-19 00:44:12 -0500

    done

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+require 'pry'
+
+def take_a_number(a, name)
+ a << name
+ puts "Welcome, #{name}. You are number #{a.length} in line."
+ a
+end
+
+def now_serving(a)
+  if a.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{a.first}."
+    a.shift
+  end
+end
+
+def line(a)
+  str = "The line is currently"
+  if a.empty?
+    puts str << " empty."
+  else
+    binding.pry puts str << ':' << a.map.with_index { |name, index| " #{index + 1}. #{name}" }.join
+  end
+end
