2dba07716231053643db0f2809a722e0438e7569
github username <github email address>
2015-11-21 01:51:55 -0500

    finish deli methods

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+#Here is my implementation, hope you like it !!!!
+# I DID :) 
+
+katz_deli = []
+
+def take_a_number(waiting_line, person)
+  waiting_line << person 
+  puts "Welcome, #{person}. You are number #{ waiting_line.index(person) + 1 } in line."
+end 
+
+def now_serving(waiting_line)
+  if waiting_line.empty? 
+    puts "There is nobody waiting to be served!"
+  else 
+    puts "Currently serving #{waiting_line.shift}."
+  end 
+end 
+
+def line(waiting_line)
+  if waiting_line.empty? 
+    puts "The line is currently empty."
+  else 
+    people_on_line = ''
+    waiting_line.each_with_index do  |person, idx| 
+      people_on_line << "#{idx + 1}. #{person} "
+    end 
+    puts "The line is currently: #{people_on_line.strip}"
+  end 
+end 
