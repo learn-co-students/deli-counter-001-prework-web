3eea9dbf919b5fb41db19fc8a99d67977eab40c9
lizkalina <liz.kalina@gmail.com>
2015-11-22 14:56:09 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,34 @@
-# Write your code here.
+#Adds person who entered deli to the line and prints their position
+def take_a_number(deli, name)
+  deli << name
+  position = deli.size
+  puts "Welcome, #{name}. You are number #{position} in line."
+end
+
+
+#Prints first person in line and removes them from line (or prints that no one is waiting, if that's the case)
+def now_serving(deli)
+  if deli.size == 0
+    puts "There is nobody waiting to be served!"
+  else
+    name = deli.shift
+    puts "Currently serving #{name}."
+  end
+end
+
+
+#Prints everyone's current place in line (or prints there's no line, if that's the case)
+def line(deli)
+
+  if deli.size == 0
+    puts "The line is currently empty."
+  else
+    line_message = ""
+    deli.each do |name|
+      position = deli.index(name) + 1
+      line_message << " #{position}. #{name}"
+    end
+    puts "The line is currently:#{line_message}"
+  end
+
+end
