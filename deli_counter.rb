1649c5cae663ebc64095913d05df2c5250eee27f
stud1nt <ocampo.izzy@gmail.com>
2015-11-24 12:57:15 -0500

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(katz_deli)
         puts "The line is currently empty."
     else
         message = "The line is currently:"
-        katz_deli.each.with_index(1) do |name, index|
+        katz_deli.map.with_index(1) do |name, index|
             message += " #{index}. #{name}"
         end 
         puts message    
@@ -26,3 +26,4 @@ def now_serving(katz_deli)
         puts "Currently serving #{katz_deli.shift}."
     end 
 end 
+
