736f3d8bfd1cd8321c3ca61af2fa8051fdeaa725
wuandy91 <wuandy91@gmail.com>
2015-11-26 14:53:45 -0500

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,34 +1,36 @@
-#people in line
 katz_deli = []
 
 def line(katz_deli)
-    #puts a string showing everyone's current place in line
-    if katz_deli.length > 0
-        puts "The line is currently: 1. #{katz_deli[0]} 2. #{katz_deli[1]} 3. #{katz_deli[2]}"
-    end
-    #puts a string if there is nobody in line
-    if katz_deli.length < 1
+    
+    people_in_line = "The line is currently:"
+
+    if katz_deli.length == 0
         puts "The line is currently empty."
+    elsif katz_deli.length > 0
+        i = 0
+        while i < katz_deli.length
+            people_in_line << " #{i+1}. #{katz_deli[i]}"
+            i+=1
+        end
+        puts people_in_line
     end
+
 end
 
-
-        
 def take_a_number(katz_deli, name)
-   	#adds a person to the line 
+
     katz_deli << name
-    #puts a string that welcomes the person and tells their position in line
-    puts "Welcome, #{name}. You are number " + (katz_deli.index(name) + 1).to_s + " in line."
+    puts "Welcome, #{name}. You are number #{katz_deli.index(name) + 1} in line."
+    
 end
 
 def now_serving(katz_deli)
-    #puts a string that calls the next person in line and then removes them from the front
-    if katz_deli.length > 0
+
+    if katz_deli.length == 0
+        puts "There is nobody waiting to be served!"
+    elsif katz_deli.length > 1
         puts "Currently serving #{katz_deli[0]}."
         katz_deli.shift
     end
-    #puts a string if there is nobody in line 
-    if katz_deli.length < 1
-        puts "There is nobody waiting to be served!"
-    end
+
 end
