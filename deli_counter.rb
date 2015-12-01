a743630bbcaff6512abcf47bed785aecb2cd1ce5
wuandy91 <wuandy91@gmail.com>
2015-12-01 17:57:55 -0500

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,36 +1,7 @@
-katz_deli = []
 
-def line(katz_deli)
-    
-    people_in_line = "The line is currently:"
+def get_a_number(line)
+    line << 1
 
-    if katz_deli.length == 0
-        puts "The line is currently empty."
-    elsif katz_deli.length > 0
-        i = 0
-        while i < katz_deli.length
-            people_in_line << " #{i+1}. #{katz_deli[i]}"
-            i+=1
-        end
-        puts people_in_line
-    end
-
-end
-
-def take_a_number(katz_deli, name)
-
-    katz_deli << name
-    puts "Welcome, #{name}. You are number #{katz_deli.index(name) + 1} in line."
-    
-end
-
-def now_serving(katz_deli)
-
-    if katz_deli.length == 0
-        puts "There is nobody waiting to be served!"
-    elsif katz_deli.length > 1
-        puts "Currently serving #{katz_deli[0]}."
-        katz_deli.shift
-    end
+    return line.length
 
 end
