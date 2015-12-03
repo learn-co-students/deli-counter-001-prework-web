6a4e60333f2979fc78103bb924d231431726781a
Christina <c.delavar@gmail.com>
2015-12-03 14:14:03 +0000

    nitrous-backup at Thu Dec  3 14:14:03 UTC 2015

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+def line(katz_deli = [])
+  if katz_deli.empty?
+     puts "The line is currently empty."
+  else
+    return katz_deli
+  end
+end
+
+def take_a_number(katz_deli = [], name)
+
+    katz_deli.push(name)
+
+end
+
+def now_serving
+  katz_deli.shift
+end
