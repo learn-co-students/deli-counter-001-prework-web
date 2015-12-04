3916c14f242d896353c1f4f6eabf944df1f78512
kaylafuchs <kfuchs24@gmail.com>
2015-12-04 14:05:46 -0500

    Done.

---
+++ runner.rb
@@ -0,0 +1,9 @@
+require_relative "deli_counter.rb"
+
+katz_deli = []
+other_deli = ["Logan", "Avi", "Spencer"]
+majestic_deli = ["Kayla","Olivia","Emily","Rex"]
+
+runner(other_deli,"Joe")
+
+#runner(majestic_deli,"Niki")
