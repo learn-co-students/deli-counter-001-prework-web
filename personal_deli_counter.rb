2da08fa1bc1ce1f5171bff1015adac667efe582f
rramdehol <rramdeholshift@gmail.com>
2015-12-15 05:46:37 -0500

    Added personal_deli_counter.rb to show how a real deli counter would operate taking the next available number in the ticketing system and not just the first number as I previously coded

--- personal_deli_counter.rb
+++ personal_deli_counter.rb
@@ -19,7 +19,20 @@ end
 def take_a_number(array, name) 
   array.push(name)  
   puts ("Welcome, #{name}. You are number #{$line_numbers[0]} in line.") 
-  #Added these lines of code to shift the removed number from the ticketing system back to the end of the array so it can cycle thorugh infinitely from 1 to 99. I used the .shift array method on the global $line_numbers array, to return the value removed from taking a ticket, and added it back to the end of global variable ($line_numbers), using the .push rray method so that the number would be recycled after the deli counter maxes out to 99 leaving the array size always full at 99 available numbers. It then updates the global array value for the next ticket to be taken with the correct order. Therefore when the next person enters into the deli they always get the first available ticket number in the array ($line_numbers[0]) and not just the first ticket as I had previously coded.   
+  #Added these lines of code to shift the removed number from the ticketing system back to the end of the array so it can cycle 
+  #thorugh infinitely from 1 to 99. I used the .shift 
+  #array method on the global $line_numbers array, 
+  #to return the value removed from taking a ticket, 
+  #and added it back to the end of global variable 
+  #($line_numbers), using the .push rray method so that 
+  #the number would be recycled after the deli counter 
+  #maxes out to 99 leaving the array size always full 
+  #at 99 available numbers. It then updates the global 
+  #array value for the next ticket to be taken with the
+  # correct order. Therefore when the next person enters 
+  #into the deli they always get the first available 
+  #ticket number in the array ($line_numbers[0]) and not 
+  #just the first ticket as I had previously coded.   
   removed_number = $line_numbers.shift
   $line_numbers = $line_numbers.push(removed_number)
 end 
