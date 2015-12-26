6c921a339b092bcc5fb7f04149edd5d7e99c620a
GHACK101 <jakegkaplan@gmail.com>
2015-12-26 09:39:58 -0500

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+def line(array)
+  if array.empty?
+    puts "The line is currently empty."
+  else
+    string = "The line is currently:"
+    for i in 0...array.size
+      string = string + " #{i+1}. #{array[i]}"
+    end
+    puts string
+  end
+end
+
+def take_a_number(array, name)
+  array << name
+  number = array.size
+  puts "Welcome, #{name}. You are number #{number} in line."
+  return name, number
+end
+
+def now_serving(array)
+  if array.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    person = array.shift
+    puts "Currently serving #{person}."
+  end
+end
