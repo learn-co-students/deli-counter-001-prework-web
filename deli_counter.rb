1cd6e24805e8083af8b4b6a7d55e80eb7723513a
Jennifer Cen <cenjennifer@gmail.com>
2016-01-12 18:48:51 +0000

    nitrous-backup at Tue Jan 12 18:48:51 UTC 2016

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,29 @@
 # Write your code here.
-katz_deli = []
+
+def get_a_number(line)
+  counter = line.count;
+
+  counter += 1;
+
+  line.push(counter);
+
+  return counter;
+
+end
+
+def serve_customer(line)
+
+  line.shift;
+  
+end
+
+
+
+
+
+
+
+
 
 def line(array)
   message = "The line is currently:"
@@ -26,8 +50,8 @@ def now_serving(array)
   if array.count == 0
     puts "There is nobody waiting to be served!"
   else
-    calloutPerson = array.first
+    calloutPerson = array.shift
     puts "Currently serving #{calloutPerson}."
-    array.shift
+
   end
 end
