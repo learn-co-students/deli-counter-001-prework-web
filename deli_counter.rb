44c578189b7582310521158419340e902088e547
WilliamMena <williammena1991@gmail.com>
2016-01-12 22:29:52 -0500

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,29 +1,67 @@
 # Write your code here.
 #make array.find_index(array[0]) your new best friend
+#array.map
 
 katz_deli = []
 
 def line(array)
-if array.length > 0
-puts "The line is currently: 1. #{array[0]} 2. #{array[1]} 3. #{array[2]}"
-# Instead of using 1. 2. 3. I can put the code below to find the actual position
-# #{array.find_index(array[0])+1}
-else
-puts "The line is currently empty." 
-end
-end
+  if array.length > 0
+    puts "The line is currently: 1. #{array[0]} 2. #{array[1]} 3. #{array[2]}"
 
+  else
+    puts "The line is currently empty." 
+  end
+end
 def take_a_number (array, name)
-array << name
-#array[-1]
-puts "Welcome, #{array[-1]}. You are number #{array.find_index(array[-1])+1} in line."
+  array << name
+
+  puts "Welcome, #{array[-1]}. You are number #{array.find_index(array[-1])+1} in line."
 end
 
 def now_serving(array)
   if array.length > 0
-  puts "Currently serving #{array[0]}."
-  array.shift
+    puts "Currently serving #{array[0]}."
+    array.shift
   else
-  puts "There is nobody waiting to be served!"
+    puts "There is nobody waiting to be served!"
+  end
 end
+
+
+
+
+
+def get_a_number(line)
+  if line == [] 
+    line << 1
+    1
+  else
+    line << (line[-1])+1
+      line[-1]
+  end
+
+
+
+
 end
+
+
+def serve_customer(line)
+  line.shift
+
+end
+
+
+
+
+
+
+
+
+
+
+
+# Instead of using 1. 2. 3. I can put the code below to find the actual position
+# #{array.find_index(array[0])+1}
+
+#array[-1]
