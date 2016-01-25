4082da53fd3a50af00b0e70982052e1eae3c3db3
Bradley Allen <bradley.allen84@gmail.com>
2016-01-25 12:12:40 +0000

    nitrous-backup at Mon Jan 25 12:12:40 UTC 2016

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,12 @@
 # Write your code here.
 def line(deli)
-	if deli.empty? == true
+	if deli.empty?
 		puts "The line is currently empty."
 	else
-		lineMsg =  "The line is currently: "	
+		lineMsg =  "The line is currently: "
 		deli.each_with_index{|val,idx|
-			lineMsg += (idx+1).to_s+". #{val} "
+			#lineMsg += (idx+1).to_s+". #{val} "
+      lineMsg += "#{idx+1}. #{val} "
 		}
 		puts lineMsg.strip!
 	end
@@ -18,10 +19,29 @@ def take_a_number(deli, name)
 end
 
 def now_serving(deli)
-	if deli.empty? == true
+	if deli.empty?
 		puts "There is nobody waiting to be served!"
 	else
 		name = deli.shift
 		puts "Currently serving #{name}."
 	end
+end
+
+
+def get_a_number(line)
+  if line.empty?
+    lineNum = 1
+  else
+    lineNum = line[line.size-1]+1
+  end
+  line << lineNum
+  return lineNum
+end
+
+def serve_customer(line)
+  if line.empty?
+    :no_customers
+  else
+    line.shift
+  end
 end
