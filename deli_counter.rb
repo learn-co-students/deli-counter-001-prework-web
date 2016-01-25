8c9950cfdb32b055b6ec2ae22e07c91fa1a01d38
xristocodos <xristojobos@gmail.com>
2016-01-25 06:56:18 -0500

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,35 @@
 # Write your code here.
+def line(katz_deli = [])
+  itr = 0
+  if katz_deli.length == 0
+    puts "The line is currently empty." 
+  else  
+    outstr = "The line is currently:"
+    while itr < katz_deli.length
+      name = katz_deli[itr]
+      posn = itr + 1
+      outstr += " #{posn}. #{name}"
+      itr += 1
+    end
+
+    puts outstr
+  end
+end
+
+def take_a_number(katz_deli = [], newCustomer)
+  queueNumber = katz_deli.size + 1
+  katz_deli.push(newCustomer)
+  puts "Welcome, #{newCustomer}. You are number #{queueNumber} in line."
+  return katz_deli
+end
+
+
+def now_serving(katz_deli)
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving " + katz_deli.shift + "."
+  end
+
+  return katz_deli
+end
