232118a33b3c8ca462af90a358d65b07e4d8d521
amandaprinzo@gmail.com <amandaprinzo@gmail.com>
2016-01-27 09:22:08 -0500

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+katz_deli = []
+
+def line(array, name = nil)
+ if name == nil
+ puts "The line is currently empty."
+ else 
+  array.each.with_index(1) do |name, index|
+    puts "The line is currently: #{index}. #{name}"
+    end 
+end
+end
+
+def take_a_number(array, name)
+array.each.with_index(1) do |name, index| 
+  if array.length == 0
+    array.push(name)
+      puts "Welcome, #{name}. You are number #{index} in line."
+  elsif
+    array.length > 0
+    array.push(name)
+      puts "Welcome, #{name}. You are number #{index} in line."
+  end 
+end
+end
+
+take_a_number(katz_deli, "Ada")
