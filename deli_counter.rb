e1c2c940081ffd6ea239419955f5eb3f99432aee
blochbr <blochbr@gmail.com>
2016-02-18 22:10:59 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,14 +2,14 @@ def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
     else
-    #current_line = []
-    names = ''
-      #katz_deli.each do |value|
-      #current_line.push(value)
-      #end
-      katz_deli.each_with_index do |value, index|
-      names += " #{index+1}. #{value}"
-      end
+     #current_line = []
+     names = ''
+     #katz_deli.each do |value|
+     #current_line.push(value)
+     #end
+     katz_deli.each_with_index do |value, index|
+     names += " #{index+1}. #{value}"
+     end
     puts "The line is currently:#{names}"
   end
 end
@@ -17,8 +17,8 @@ end
 def take_a_number(katz_deli, name)
     #katz_deli.size == 0
     katz_deli.push(name)
-    index_plus_one = katz_deli.index(name) + 1
-    puts "Welcome, #{name}. You are number #{index_plus_one} in line."
+    # index_plus_one = katz_deli.index(name) + 1
+    puts "Welcome, #{name}. You are number #{katz_deli.index(name) + 1} in line."
  # else
   #  katz_deli.push(name)
   #  index_plus_one = katz_deli.index(name) + 1
