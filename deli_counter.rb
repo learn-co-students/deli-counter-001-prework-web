1911325a80ffa1a3a73f02c31b53f74c34a26b45
dcarrano <dcarrano@gmail.com>
2016-02-18 14:46:29 +0000

    nitrous-backup at Thu Feb 18 14:46:29 UTC 2016

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,28 +1,20 @@
-katz_deli = []
+def get_a_number(line)
+  #position = line.length + 1
 
-def take_a_number(katz_deli, new_name)
-  new_number = katz_deli.length + 1
-  katz_deli.push(new_name)
-  puts "Welcome, #{new_name}. You are number #{new_number} in line."
-end
-
-def now_serving(katz_deli)
-  if katz_deli.length == 0 then
-    puts "There is nobody waiting to be served!"
+  if line.empty?
+    position = 1
   else
-    puts "Currently serving #{katz_deli.first}."
-    katz_deli.shift
+    position = line.last + 1
   end
+
+  line.push(position)
+
+  position
+
 end
 
-def line(katz_deli)
-  if katz_deli.length == 0 then
-    puts "The line is currently empty."
-  else
-    current_line = "The line is currently:"
-    katz_deli.each_with_index do |name, position|
-      current_line += " #{(position + 1)}. #{name}"
-    end
-    puts current_line
-  end
+def serve_customer(line)
+  line.shift
 end
+
+
