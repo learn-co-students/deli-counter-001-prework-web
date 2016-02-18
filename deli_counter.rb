a353b38199e66d9161b33a5e1fcca3d8ee21d8ad
blochbr <blochbr@gmail.com>
2016-02-18 22:05:56 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,12 +2,12 @@ def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
     else
-    current_line = []
+    #current_line = []
     names = ''
-      katz_deli.each do |value|
-      current_line.push(value)
-      end
-      current_line.each_with_index do |value, index|
+      #katz_deli.each do |value|
+      #current_line.push(value)
+      #end
+      katz_deli.each_with_index do |value, index|
       names += " #{index+1}. #{value}"
       end
     puts "The line is currently:#{names}"
@@ -15,15 +15,15 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
-  if katz_deli.size == 0
+    #katz_deli.size == 0
     katz_deli.push(name)
     index_plus_one = katz_deli.index(name) + 1
     puts "Welcome, #{name}. You are number #{index_plus_one} in line."
-  else
-    katz_deli.push(name)
-    index_plus_one = katz_deli.index(name) + 1
-    puts "Welcome, #{name}. You are number #{index_plus_one} in line."
-  end
+ # else
+  #  katz_deli.push(name)
+  #  index_plus_one = katz_deli.index(name) + 1
+  #  puts "Welcome, #{name}. You are number #{index_plus_one} in line."
+  #end
 end
 
 def now_serving(katz_deli)
@@ -34,3 +34,23 @@ def now_serving(katz_deli)
     puts "Currently serving #{serving}."
   end
 end
+
+ katz_deli = []
+
+  take_a_number(katz_deli, "Ada") #=> 1
+  take_a_number(katz_deli, "Grace") #=> 2
+  take_a_number(katz_deli, "Kent") #=> 3
+
+ line(katz_deli) #=> "The line is currently: 1. Ada 2. Grace 3. Kent"
+
+  now_serving(katz_deli) #=> "Currently serving Ada."
+
+  line(katz_deli) #=> "The line is currently: 1. Grace 2. Kent"
+
+  take_a_number(katz_deli, "Matz") #=> 3
+
+  line(katz_deli) #=> "The line is currently: 1. Grace 2. Kent 3. Matz"
+
+  now_serving(katz_deli) #=> "Currently serving Grace."
+
+  line(katz_deli) #=> "The line is currently: 1. Kent 2. Matz"
