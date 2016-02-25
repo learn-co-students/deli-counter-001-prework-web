5e1899419d946354f98ee9afe3459df1c13141c6
supermancbs <clark.sacktor@gmail.com>
2016-02-24 21:10:48 -0500

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,38 @@
 # Write your code here.
+
+katz_deli=[]
+
+def line(katz_deli)
+	if katz_deli.any? == false
+		puts "The line is currently empty."
+		return "The line is currently empty."
+	end 
+	arr=["The line is currently:"]
+	for i in 0..(katz_deli.size-1)
+		arr.push (i+1).to_s+ ". " + katz_deli[i].to_s 
+	end 
+	puts arr.join(" ")
+end 
+
+
+
+def take_a_number(katz_deli, name)
+	katz_deli.push name 
+	puts "Welcome, #{katz_deli[katz_deli.size-1]}. You are number #{katz_deli.size} in line."
+
+
+end 
+
+
+
+
+
+def now_serving(katz_deli)
+	if katz_deli.any? == false
+		puts "There is nobody waiting to be served!"
+		return "There is nobody waiting to be served!"
+	end 
+	puts "Currently serving #{katz_deli[0]}."
+	katz_deli.shift
+
+end 
