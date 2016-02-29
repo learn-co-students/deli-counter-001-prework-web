14d1e9df2ec336b4fcca9181134805aea031c625
jjseymour <jjseymour@gmail.com>
2016-02-29 18:03:56 -0500

    add .object_id hash to deli_counter.rb to fix issues

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,233 +1,119 @@
-# class Deli
+require "pry"
 
 def line(array)
-  #order = []
-  #number = 1
-  counter = 0
-  if array == []
+  if array.empty?
     puts "The line is currently empty."
-  else
-    #puts "The line is currently: #{array.count}. "
-    last = ""
-    full = ""
-    until counter == array.count
-      order = " #{counter + 1}. #{array[counter]}"
-      full = last + order
-      last = full
-      counter += 1
-    #array.collect do |patron|
-      #order << "#{number}. #{patron}"
-      #number += 1
+  else 
+    full_line = []
+    array.each_with_index do |name, index|
+      full_line << "#{index + 1}. #{name}"
     end
-    #order.flatten
-    puts "The line is currently:#{full}"
+    string = full_line.join(" ")
+    puts "The line is currently: #{string}"
   end
 end
 
-def take_a_number(array, name)
-  if array == []
-    array.unshift(name)
-    puts "Welcome, #{name}. You are number #{array.count} in line."
-  else
-    array.push(name)
-    puts "Welcome, #{name}. You are number #{array.count} in line."
-  end
+def take_a_number(array_line, name_st)
+  array_line << name_st
+  puts "Welcome, #{name_st}. You are number #{array_line.count} in line."
 end
 
-def now_serving(array)
-  if array == []
+def now_serving(array_line)
+  if array_line.empty?
     puts "There is nobody waiting to be served!"
-  else
-    puts "Currently serving #{array[0]}."
-    array.shift
+  else 
+    puts "Currently serving #{array_line[0]}."
+    array_line.shift
   end
-  #nobody = puts "There is nobody waiting to be served!"
-  #people = puts "Currently serving #{array[0]}."
-  #array == [] ? nobody : people
-  #array.shift
 end
 
-# $get_a_number_global = []
-# def full_array(last_array, integer = 1)
-#   last_array = last_array << integer
-#   puts "last_array is #{last_array}"
-#   # return last_num
-# end
-
-# def full_hash
-#   deli_hash = {:position0 => 0}
-#   puts "deli_hash is #{deli_hash}"
-# end
-
-# def array_helper
-#   counter = yield
-#   if yield == 0
-#     counter = 0
-#     counter += 0
-#   elsif yield == 1
-#     counter += 1
-#   elsif yield == -1
-#     counter -= 1
-#   end
-#   puts "counter is #{counter}"  
-#   counter
-# end
-
-# helper = 0
-# array_helper do |x|
-#   helper = 0
-# end
-
-@@global_get_a_number = 0
-# @@global_serve_customer = 0
-# @@global_array_nil = 0
-@@global_returned_nil = 0
-
+@@line_counter = {}
 
 def get_a_number(array)
-  @@global_get_a_number += 1
-  # puts "global_get_a_number is #{@@global_get_a_number}"
-  # puts "global_serve_customer is #{@@global_serve_customer}"
-  # puts "global_array_nil is #{@@global_array_nil}"
-  # puts "global_returned_nil is #{@@global_returned_nil}"
-  if @@global_returned_nil == 1
-    number = @@global_get_a_number
-    array << number
-  else
-    if array == []
-      @@global_get_a_number = 1
-      # @@global_array_nil += 1
-      number = array.count + 1
-      array << number
+  if array.empty? 
+    if @@line_counter[array.object_id] == nil
+      array << 1
     else
-      number = array.last + 1
-      array << number
+      array << @@line_counter[array.object_id]
     end
+  else
+    array << (array.last + 1)
   end
-  # puts "array.count is #{array.count}"
-  # array_helper_inside = array_helper do |x|
-  #   x = array.count
-  # end
-  # puts "array_helper_inside #{array_helper_inside}"
-  # global = $get_a_number_global
-  # if global == [] && array == []
-  #   number = array.count + 1
-  #   array << number
-  # elsif array == [] && global != []
-  #   puts "global inside elsif #{global.last}"
-
-  #   number = global.last + 1
-  #   array << number
-  # else
-  #   number = array.last + 1
-  #   array << number
-  # end
-  # $get_a_number_global << array.last
-  # puts "array is #{array}"
-  # puts "global array is #{$get_a_number_global}"
-
-  # deli_array = full_array(array)
-  # puts "deli_array #{deli_array}"
-  # number = 0
-  # if block_given?  
-  #   array.each do |x|
-  #     yield x
-  #   end
-  # end
-  # if block_given?
-  #   yield
-  # end
-  # puts "yield is #{yield}"
-
-  # if array == []
-  #   @@global_array_nil += 1
-  #   number = array.count + 1
-  #   array << number
-  # else
-  #   number = array.last + 1
-  #   array << number
-  # end
-
-  # {|x| x << number}
-  #   puts "array is #{array}"
-  #   puts "array 1 to -1 is #{array[1..-1]}"
-  # return array[1..-1]
-  # new_array = Array.new
-  # array.each do |x|
-  #   # new_array = Array.new << x
-  #   new_array << x + 1
-  #   puts "new array is #{new_array}"
-  # end
-  # puts "new array outside .each is #{new_array}"
-  
-  number
-  # puts "number is #{number}"
-  # puts "array is #{array}"
+  # binding.pry
+  return array.last
 end
 
 def serve_customer(array)
-  # @@global_serve_customer += 1
-  # global = $get_a_number_global.shift
-  # puts "global in serve is #{$get_a_number_global}"
-  # full_array(array.last)
-  # array_for_full = []
-  take_off = array.shift
-  # puts "take_off #{take_off}"
-  # full_hash.merge!({:position1 => take_off})
-  # puts "full_hash #{full_hash}"
-  # array_for_full = array_for_full << take_off
-  # full_array(array_for_full, take_off)
-  # puts "array_for_full #{array_for_full}" 
-  if array == []
-    @@global_returned_nil += 1
+  # binding.pry
+  if array.length == 1
+    @@line_counter[array.object_id] = (array.shift + 1)
+  else
+    @@line_counter[array.object_id] = 0
+    array.shift
   end
-  return take_off
-  # take_off = array[1..-1]  
-  #   puts "array is #{array}"
-  #   puts "array 1 to -1 is #{array[1..-1]}"
-  # return array[0]
 end
 
-# holding_array = [0]
-
-# get_a_number(holding_array) do |array|
-#   puts "#{array}"
+# def line(array)
+#   counter = 0
+#   if array == []
+#     puts "The line is currently empty."
+#   else
+#     last = ""
+#     full = ""
+#     until counter == array.count
+#       order = " #{counter + 1}. #{array[counter]}"
+#       full = last + order
+#       last = full
+#       counter += 1
+#     end
+#     puts "The line is currently:#{full}"
+#   end
 # end
 
-# puts "holding_array is #{holding_array}"
+# def take_a_number(array, name)
+#   if array == []
+#     array.unshift(name)
+#     puts "Welcome, #{name}. You are number #{array.count} in line."
+#   else
+#     array.push(name)
+#     puts "Welcome, #{name}. You are number #{array.count} in line."
+#   end
+# end
 
-# array = []
-# array << get_a_number(array)
-# array << get_a_number(array)
-# array << get_a_number(array)
-# array << get_a_number(array)
-# puts "Array is #{array}"
+# def now_serving(array)
+#   if array == []
+#     puts "There is nobody waiting to be served!"
+#   else
+#     puts "Currently serving #{array[0]}."
+#     array.shift
+#   end
+# end
 
-# line = []
-# get_a_number(line)
-# puts "line is #{line}"
-# get_a_number(line)
-# puts "line is #{line}"
-# serve_customer(line)
-# puts "line is #{line}"
-# serve_customer(line)
-# puts "line is #{line}"
-# get_a_number(line)
-# puts "line is #{line}"
-# get_a_number(line)
-# puts "line is #{line}"
+# @@global_get_a_number = 0
+# @@global_returned_nil = 0
 
-# array = [1, 2, 3, 4]
-# get_a_number(array)
-# get_a_number(array)
-# array = []
-# get_a_number(array)
-# puts "get_a_number is #{get_a_number}"
+# def get_a_number(array)
+#   @@global_get_a_number += 1
+#   if @@global_returned_nil == 1
+#     number = @@global_get_a_number
+#     array << number
+#   else
+#     if array == []
+#       @@global_get_a_number = 1
+#       number = array.count + 1
+#       array << number
+#     else
+#       number = array.last + 1
+#       array << number
+#     end
+#   end  
+#   number
+# end
 
-
-# counter = 0
-# def last_in_line(array = 0)
-#    number_final = array << array.count
-
-
+# def serve_customer(array)
+#   take_off = array.shift
+#   if array == []
+#     @@global_returned_nil += 1
+#   end
+#   return take_off
 # end
--- spec/additional_spec.rb
+++ spec/additional_spec.rb
@@ -1,41 +1,73 @@
 describe "Additional Tests" do
-  it "gives a user a number based on when they enter the store" do
-    line = []
-    position = get_a_number(line)
-    expect(position).to eq(1)
-    expect(line).to eq([1])
+  context "#get_a_number" do
+     it "adds newest customer to the end of the queue" do
+      queue = []
+      get_a_number(queue)
+      expect(queue).to eq([1])
+      
+      get_a_number(queue)
+      expect(queue).to eq([1,2])
+    end
+    
+    it "returns the customer's ticket number" do
+      queue = []
+      ticket_number = get_a_number(queue)
+      expect(ticket_number).to eq(1)
+    end
   end
 
-  it "knows how to take people off of the line" do
-    line = []
-    expect(get_a_number(line)).to eq(1)
-    expect(get_a_number(line)).to eq(2)
-    expect(serve_customer(line)).to eq(1)
-    expect(line).to eq([2])
+  context "#serve_customer" do
+    it "knows how to take people off of the queue" do
+      queue = []
+      expect(get_a_number(queue)).to eq(1)
+      expect(get_a_number(queue)).to eq(2)
+
+      expect(serve_customer(queue)).to eq(1)
+
+      expect(queue).to eq([2])
+    end
   end
 
-  it "can deal with a busy store" do
-    line = []
-    get_a_number(line)
-    get_a_number(line)
+  context "Busy Store" do
+    it "can deal with a busy store" do
+      queue = []
+      get_a_number(queue)
+      get_a_number(queue)
 
-    serve_customer(line)
+      serve_customer(queue)
 
-    get_a_number(line)
-    expect(line).to eq([2,3])
-  end
+      get_a_number(queue)
+      expect(queue).to eq([2,3])
+    end
 
-  it "can deal with a store's customer waves" do
-    line = []
-    get_a_number(line)
-    get_a_number(line)
-    serve_customer(line)
-    serve_customer(line)
-    expect(line).to eq([])
-    # nothing for 3 hours
+    it "can deal with a store's customer waves" do
+      queue = []
+      get_a_number(queue)
+      get_a_number(queue)
+      serve_customer(queue)
+      serve_customer(queue)
+      expect(queue).to eq([])
+      # nothing for 3 hours
 
-    get_a_number(line)
-    get_a_number(line)
-    expect(line).to eq([3,4])
+      get_a_number(queue)
+      get_a_number(queue)
+      expect(queue).to eq([3,4])
+    end
+
+    it "can deal with 2 stores" do
+      deli1 = []
+      get_a_number(deli1)
+      get_a_number(deli1)
+      serve_customer(deli1)
+      expect(deli1).to eq([2])
+
+      deli2 = []
+      get_a_number(deli2)
+      get_a_number(deli2)
+      get_a_number(deli2)
+      get_a_number(deli2)
+      serve_customer(deli2)
+      expect(deli2).to eq([2,3,4])
+    end
   end
 end
