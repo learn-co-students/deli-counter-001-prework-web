9809f29f6c24ff9f18b5e301e431803a03b26f51
molotov2k <molotov2k@gmail.com>
2016-03-01 10:22:29 -0500

    re-completed the lab

--- origin_version.rb
+++ origin_version.rb
@@ -1,21 +1,22 @@
+katz_deli = []
+
 def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
   else
-    current_line = "The line is currently: "
+    indexed_line = []
     katz_deli.each_with_index do |name, index|
-      current_line << "#{index + 1}. #{name} "
+      indexed_name = "#{index+1}. #{name}"
+      indexed_line.push indexed_name
     end
-    puts current_line.chop
+    puts "The line is currently: #{indexed_line.join(" ")}"
   end
 end
 
-
 def take_a_number(katz_deli, name)
-  katz_deli << name
+  katz_deli.push name
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
-
 
 def now_serving(katz_deli)
   if katz_deli.empty?
@@ -25,4 +26,3 @@ def now_serving(katz_deli)
     katz_deli.shift
   end
 end
-
