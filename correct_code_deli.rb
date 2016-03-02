41e8e129511dc7f07d0f109442950f9de462d130
jonalexander <jonathandalexander@gmail.com>
2016-03-02 14:19:14 -0500

    Tweaked comments for more clarity since I had completed this lab a while back.

--- correct_code_deli.rb
+++ correct_code_deli.rb
@@ -1,27 +1,25 @@
+#correct code
+
 katz_deli = []
 
 def line(katz_deli)
-	###soft code notes
-	#print out a string displaying each person and their place in line
-	#iterate through each array value and << to one string
-	#print message if array is empty
-	#index value can be incremented by 1 to accurately reference place in line
+	#soft code notes
+	#grab numbers of items in KD array
+	#print 'The line is... ' + index + name + index(2) + name(2) + index(3) + name(3)
+	#or create an intro string 'The line is currently:' >>index1/name1
+	#keep building upon string for each iteration
 
 	line_msg = "The line is currently:"
-	#line_msg will serve as the base string which we will add names to 
-	#when generating the final list
 
 	if katz_deli.any? == false
 	puts "The line is currently empty."
-	#the .any? method returns true if the array contains 
-	#values or false if its empty
+	#the .any? method returns true if the array contains values or false if its empty
 
 	else
 		katz_deli.each_with_index do |name, index|
 
 			real_index = index + 1 		
-			#to properly display the names in the list i add 1 to the original 
-			#index value {1. -- 2. -- 3. --}
+			#to properly display the names in the list i add 1 to the original index value {1. -- 2. -- 3. --}
 
 			name_for_list = " #{real_index}. #{name}"
 			#here i am generating a small string that i will add to #line_msg below
@@ -38,9 +36,9 @@ end
 
 
 def take_a_number(katz_deli, name)
-	###soft code notes
-	#determine index position (and line position) of #name parameter
-	#print message with #name and #real_index
+	#soft code notes
+	#print message with #name and #index
+	#determine index of name
 
 	katz_deli << name	
 	#adds #name to array
@@ -54,10 +52,6 @@ end
 ###
 
 def now_serving(katz_deli)
-	###soft code notes
-	#print message if array is empty
-	#call out the next person in line (aka first person in array)
-	#remove that same name from array
 
 	if katz_deli.any? == false
 	puts "There is nobody waiting to be served!"
@@ -65,12 +59,8 @@ def now_serving(katz_deli)
 	
 	else
 		current_customer = katz_deli.first
-		#.first will grab the first value in KD array
-
 		puts "Currently serving #{current_customer}."
-
 		katz_deli.shift
-		#.shift removes first item from array
-
-	end 
+		
+	end #if loop
 end
