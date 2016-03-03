32d2048fd7764a6c47bc4056635e99e10efb6cdb
jaspercurry <jaspercurry@gmail.com>
2016-03-02 23:05:40 -0500

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,51 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+
+  if katz_deli.empty? == true
+    puts "The line is currently empty."
+  else
+
+array = []
+katz_deli.each.with_index(1) do |name, index|
+
+array << " #{index}. #{name}"
+
+end
+puts "The line is currently:#{array*""}"
+end
+end
+
+
+def take_a_number(katz_deli, new)
+  x = katz_deli.size + 1
+#look into brining in the array size with x = katz_deli.size?
+  katz_deli << new
+
+  puts "Welcome, #{new}. You are number #{x} in line."
+
+end
+
+def now_serving(katz_deli)
+    if katz_deli.empty? == true
+    puts "There is nobody waiting to be served!"
+  else
+  front = katz_deli.shift
+puts "Currently serving #{front}."
+end
+end
+
+
+
+def nothing #test code for take a number
+  katz_deli.each.with_index(1) do |name, index|
+    puts "Welcome, #{name}. You are number #{index} in line."
+
+  end
+
+end
+
+#http://stackoverflow.com/questions/3500814/ruby-array-to-string-conversion
+#http://stackoverflow.com/questions/20258086/difference-between-each-with-index-and-each-with-index-in-ruby
+#source for some of my thought process
+
