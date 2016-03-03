d9ab2e0cf37df3307df432520066c7334a1e8c71
snlamm <shmuel.lamm@gmail.com>
2016-03-03 14:17:48 -0500

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,23 +2,18 @@
 
 def line (katz_deli)
     if katz_deli.size < 1 
-      puts "The line is currently empty." if katz_deli.size < 1
+      puts "The line is currently empty." 
     else
-     first_part = katz_deli.collect.with_index do |i, index|
+     line_order = katz_deli.collect.with_index do |i, index|
       "#{index +1}. #{i}"
     end
-     puts "The line is currently: #{first_part.join(" ")}"
+     puts "The line is currently: #{line_order.join(" ")}"
   end
 end
 
-def take_a_number (katz_deli, name)
-  katz_deli.push(name)
-  line_length = katz_deli.size
-  if katz_deli == 1
-    puts "Welcome, #{name}. You are number 1 in line."
-  else 
-    puts "Welcome, #{name}. You are number #{line_length} in line."
-  end
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end
 
 def now_serving (katz_deli)
