ddb5580b8698cea867f00d0c13855e69fb658791
pressstart101 <walterscott404@yandex.com>
2016-03-06 04:34:29 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+katz_deli = []
+
+def line(arr)
+  if arr.count == 0
+  puts "The line is currently empty."
+    else
+    message = "The line is currently:"
+  arr.each_with_index do |x, y| message += " #{y+1}. #{x}"
+  end
+  puts message
+end
+end
+
+def take_a_number(arr, name)
+  arr.push(name)
+  puts "Welcome, #{name}. You are number #{arr.count} in line."
+end
+
+def now_serving(arr)
+  if arr.count == 0
+    puts "There is nobody waiting to be served!"
+    else
+    puts "Currently serving #{arr.shift}."
+  end
+end
