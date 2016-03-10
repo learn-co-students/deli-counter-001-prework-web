9d4dfb2eabb069702b245198b51bf1f6baf6895b
aharshbe <aharshbe@live.com>
2016-03-10 12:18:29 -0800

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -26,3 +26,30 @@ end
 
 
 #Hey guys I'm really struggling, not sure what I'm doing wrong. Any help? :)
+
+
+
+#attempts at trying to do second method:
+
+
+def take_a_number(katz_deli, name)
+    
+   
+   people_waiting =  katz_deli.count
+    
+        
+        while people_waiting > 0
+        
+        
+        puts "#{name} your place in line is #{people_waiting+1}"
+        
+        
+        break
+        
+        
+        end
+    
+    end
+
+
+take_a_number(katz_deli = ['jane', 'john', 'alfted'],'Ada')
