f5b555e0d149dcb99e8a4da0856016527596ee26
KevinDTran <kevin@zipmark.com>
2016-03-12 01:14:17 -0600

    Completed

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,5 @@
 katz_deli = []
 
-
 def line(katz_deli)
   positions = ""
   if katz_deli.count < 1
@@ -15,15 +14,12 @@ def line(katz_deli)
   end
 end
 
-
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
-  katz_deli.each_with_index do |person, index|
-    position = "#{index + 1}"
-    return position
+  position = katz_deli.index(name) + 1
   puts "Welcome, #{name}. You are number #{position} in line."
+  return katz_deli
 end
-
 
 def now_serving(katz_deli)
   if katz_deli.count > 0
