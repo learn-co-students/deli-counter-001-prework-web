64911353b56034494cde6cc4a756a308d3838725
bensu1013 <bensu1013@hotmail.com>
2016-03-17 04:40:00 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+def line(array)
+  if array.length > 1
+    sentence = "The line is currently:"
+    array.each {|name|
+    sentence += " #{array.index(name) + 1}. #{name}" }
+    puts sentence
+  else
+    puts "The line is currently empty."
+  end
+end
+
+def take_a_number(array, name)
+  array.push(name)
+  puts "Welcome, #{name}. You are number #{array.size} in line."
+end
+
+def now_serving(array)
+  if array.length > 1
+    puts "Currently serving #{array.first}."
+    array.shift
+  else
+    puts "There is nobody waiting to be served!"
+  end
+end
