1d4b1b7dec825583b002f180e193850136792914
Derick Quartey <niikwartey1@gmail.com>
2016-03-25 16:32:53 +0000

    nitrous-backup at Fri Mar 25 16:32:53 UTC 2016

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,12 @@
 # Write your code here.
+# #line takes a array of customers and prints their names and number in order
 def line(customers)
+  # initiate line_info prior to finding the info
   line_info = ""
+
+  ## find out if there's actually a customer
+  # if not, line info should reflect that
+  # Otherwise, line info should list customer(s) with the position in line
   if customers.size == 0
     line_info = "The line is currently empty."
   else
@@ -10,11 +16,13 @@ def line(customers)
   puts line_info
 end
 
+###ake_a_number adds a customer to the line and assigns them a number
 def take_a_number(customers, name)
   customers.push(name)
   puts "Welcome, #{name}. You are number #{customers.index(name) + 1} in line."
 end
 
+# #now_serving tells what customer is being served
 def now_serving(customers)
   if customers.size == 0
     puts "There is nobody waiting to be served!"
@@ -23,4 +31,3 @@ def now_serving(customers)
     customers.delete(customers.first)
   end
 end
-
