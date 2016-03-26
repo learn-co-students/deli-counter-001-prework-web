73804fee23468ee2cb753189bf4fb57c2ece85d1
JennyShalai <jenny.shalai@gmail.com>
2016-03-25 22:07:15 -0400

    code adapted according new knowledges for the last month

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,33 +1,27 @@
 # Write your code here.
 
-
-
 def line(array)
-  string=[]
-  if array.length == 0
+  if array.empty?
     puts "The line is currently empty."
   else
-    array.each_with_index{|array, i|string.push( "#{i + 1}. #{array}");}
-    new_string = string.join(" ")
-    puts "The line is currently: " + new_string
+    foo = []
+    array.each_with_index {|array, i|
+      foo.push( "#{i + 1}. #{array}")
+    }
+    puts "The line is currently: " + foo.join(" ")
   end
 end
 
-
-
-def take_a_number(array,new_customer)
-  new_array=array.push(new_customer)
-  a = new_array.length
-  puts "Welcome, #{new_customer}. You are number #{a} in line."
-
+def take_a_number(array, customer)
+  array.push(customer)
+  puts "Welcome, #{customer}. You are number #{array.length} in line."
 end
 
 def now_serving(array)
-  a = array.length
-  if a == 0 
+  if array.empty?
     puts "There is nobody waiting to be served!"
   else 
-    puts "Currently serving #{array[0]}."
+    puts "Currently serving #{array.first}."
     array.shift
   end
 end
