678a6de404115cfeccea6cfb5fd9128759e158d5
mlombard82 <mlombard82@gmail.com>
2016-03-31 13:09:52 -0400

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,12 @@
+katz_deli = []
+
 def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
     customers = "The line is currently:"
-    katz_deli.each_with_index do |customer, index|
-      customers << " #{index + 1}. #{customer}"
+    katz_deli.each_with_index do |customer, index|  # enumerator method that keeps track of the index number of the current iteration. it takes 2 arguments.
+      customers << " #{index + 1}. #{customer}" #string interpolation. It replaces placeholders within a string with values they represent.
     end
     puts customers
   end
