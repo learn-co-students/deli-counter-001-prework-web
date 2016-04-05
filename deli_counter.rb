c2c62de7ca3997b396775d3ffa1317c9922f84e9
supermancbs <clark.sacktor@gmail.com>
2016-04-04 21:28:44 -0400

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,38 +1,32 @@
 # Write your code here.
 
-katz_deli=[]
+katz_deli = []
+
 
 def line(katz_deli)
-	if katz_deli.any? == false
+	result="The line is currently:"
+	if katz_deli.empty?
 		puts "The line is currently empty."
-		return "The line is currently empty."
+	else 
+		katz_deli.each_with_index do |name, index|
+			result+=" #{index+1}. #{name}"
+		end
+		puts result 
 	end 
-	arr=["The line is currently:"]
-	for i in 0..(katz_deli.size-1)
-		arr.push (i+1).to_s+ ". " + katz_deli[i].to_s 
-	end 
-	puts arr.join(" ")
 end 
 
 
-
-def take_a_number(katz_deli, name)
-	katz_deli.push name 
-	puts "Welcome, #{katz_deli[katz_deli.size-1]}. You are number #{katz_deli.size} in line."
-
-
+def take_a_number(katz_deli, name) 
+	katz_deli << name 
+	puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end 
-
-
-
 
 
 def now_serving(katz_deli)
-	if katz_deli.any? == false
+	if katz_deli.empty?
 		puts "There is nobody waiting to be served!"
-		return "There is nobody waiting to be served!"
+	else 
+		puts "Currently serving #{katz_deli.first}."
+		katz_deli.shift
 	end 
-	puts "Currently serving #{katz_deli[0]}."
-	katz_deli.shift
-
 end 
