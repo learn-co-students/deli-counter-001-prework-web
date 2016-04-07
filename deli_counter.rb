d7f0894e8c416678f2e3711ed062518c3b324a5e
Mario245 <mario.peart@live.com>
2016-04-07 14:36:17 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,30 +4,28 @@ def take_a_number(deli_array, name)
 end
 
 def shuffle(deli_array)
+  value = ""
   deli_array.each_with_index do |deli, idx|
-    print " #{idx+1}. #{deli}"
+    value = value + " #{idx+1}. #{deli}"
+
   end
+   puts "The line is currently:" + value
 end
-
-
-
 
 def line(deli_array)
   if deli_array.size > 0
-    print "The line is currently:"
     shuffle(deli_array)
-    puts
   else
     puts "The line is currently empty."
-end
+  end
+ end
 
 def now_serving(deli_array)
 	if deli_array.size > 0
 	 puts "Currently serving #{deli_array.shift}."
 	else
-     puts "There is nobody waiting to be served!"
+   puts "There is nobody waiting to be served!"
   end
-end
 end
 
   katz_deli = []
@@ -35,6 +33,10 @@ end
   take_a_number(katz_deli, "Ada") #=> 1
   take_a_number(katz_deli, "Grace") #=> 2
   take_a_number(katz_deli, "Kent") #=> 3
+  take_a_number(katz_deli, "Logan") #=> 4
+  take_a_number(katz_deli, "Avi") #=> 3
+  take_a_number(katz_deli, "Spencer") #=> 3
+
 
   line(katz_deli)
 
@@ -50,3 +52,30 @@ end
 
   line(katz_deli) #=> "The line is currently: 1. Kent 2. Matz"
 
+  line(katz_deli)
+
+  now_serving(katz_deli) #=> "Currently serving Ada."
+
+  line(katz_deli) #=> "The line is currently: 1. Grace 2. Kent"
+
+
+
+  line(katz_deli) #=> "The line is currently: 1. Grace 2. Kent 3. Matz"
+
+  now_serving(katz_deli) #=> "Currently serving Grace."
+
+  line(katz_deli)
+
+  line(katz_deli)
+
+  now_serving(katz_deli) #=> "Currently serving Ada."
+
+  line(katz_deli) #=> "The line is currently: 1. Grace 2. Kent"
+
+
+
+  line(katz_deli) #=> "The line is currently: 1. Grace 2. Kent 3. Matz"
+
+  now_serving(katz_deli) #=> "Currently serving Grace."
+
+  line(katz_deli)
