e3e7c1ed753bc1b9d9bf7e4c126ed5b87251c2e0
blessedsoy <unmigong@gmail.com>
2016-04-12 16:20:06 -0400

    Deli Counter

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,31 @@
 # Write your code here.
+katz_deli = []
+
+def line (katz_deli)
+  if katz_deli.size == 0
+    puts "The line is currently empty."
+  else 
+     result = "The line is currently:" 
+     katz_deli.map.with_index {|x, i| 
+     result << " #{i + 1}. #{x}"
+      }
+      puts result
+  end
+end
+
+def take_a_number (katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
+end
+
+
+def now_serving (katz_deli)
+  if katz_deli.size == 0
+    puts "There is nobody waiting to be served!"
+  else
+  puts "Currently serving #{katz_deli[0]}."
+  katz_deli.shift
+  end
+end
+
+
