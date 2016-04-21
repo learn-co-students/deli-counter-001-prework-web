6fab902d4bf83c2ce749ccca1da220a193227b3a
wvazquez <vazquezwilmer@gmail.com>
2016-04-21 01:16:58 -0400

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,24 +1,21 @@
 def line(customers)
-  if customers == []
+  if customers.empty?
     puts  "The line is currently empty."
   else
     list = ""
-    customers.each_with_index do |name, index|
-      list += "#{index+1}. #{name} "
-    end
-    puts "The line is currently: " + list.strip
+    customers.each_with_index { |name, index| list += " #{index+1}. #{name}" }
+    puts "The line is currently:" + list
   end
-
 end
 
 def take_a_number(customers, person)
   customers.push(person)
-  puts "Welcome, #{person}. You are number #{customers.index(person)+1} in line."
+  puts "Welcome, #{person}. You are number #{customers.length} in line."
 end
 
 def now_serving(customers)
-  if customers == []
-    puts "There is nobody waiting to be served!"
+  if customers.empty?
+    puts "There is nobody waiting to be served!" 
   else
     puts "Currently serving #{customers.shift}."
   end
