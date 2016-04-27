e24537d1cc7180622cb52413a524c8d2f7bedbde
Mauricio Lombard <mlombard82@gmail.com>
2016-04-27 07:45:05 -0400

    Update deli_counter.rb

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,8 +15,8 @@ end
 
 
 def take_a_number(katz_deli, name)
-  katz_deli << "#{name}"
-  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+  katz_deli << name
+  puts "#{katz_deli.length}"
 end
 
 
