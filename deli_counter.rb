6423b9d6c7498a3b12a67b07012d15ff74e0f75e
dhechagas <dhechagas@gmail.com>
2016-04-28 23:41:15 -0400

    Done

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,17 +1,17 @@
 # Write your code here.
-def line(array) # 'line' is a method that receive a varialbe 'array'
+def line(array)
   if array.empty?
     puts "The line is currently empty."
   else
-    ret = "The line is currently:"     # 'ret' is a variable created with string 'The line is currently'
+    ret = "The line is currently:"
     count = 0
 
-    for name in array do  #'name'is a variable created
-      count += 1 # increment
-      ret << " #{count}. #{name}" # 'ret is return add'
+    for name in array do
+      count += 1
+      ret << " #{count}. #{name}"
     end
 
-    puts ret # 'ret' is return complete The line is currently: 1. name, 2. name, 3. name
+    puts ret
   end
 end
 
@@ -25,7 +25,7 @@ def now_serving(array)
   if array.empty?
     puts "There is nobody waiting to be served!"
   else
-    name = array.shift # tira a primeira pessoa da fila e retorna ela
-    puts "Currently serving #{name}." # coloquei a pessoa na variable 'name' e coloquei numa strign
+    name = array.shift
+    puts "Currently serving #{name}."
   end
 end
