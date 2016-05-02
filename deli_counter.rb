d3a757fcd54a3ad317a9218737ea9b2ae964c5e2
Ahmet Calis <calisahmet@gmail.com>
2016-05-01 21:18:37 -0400

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,50 @@
 # Write your code here.
+def line(deli)
+  if deli.empty? #this conditional checks whether line is empty
+    puts "The line is currently empty." #
+  else
+    current_line = "The line is currently:" 
+    deli.each.with_index(1) do |person, i| # calls .each and .with_index with and argument methods on deli and iterate over each element
+      current_line << " #{i}. #{person}" # wraps i and person variables for string interpolation and writes it in to current_line
+  end
+  puts current_line 
+  end
+  
+end
+
+def take_a_number(deli, name)
+  number = deli.length + 1 # line starts from 1
+  deli << name # shovelling new name to the array
+  puts "Welcome, #{name}. You are number #{number} in line." # welcomes the new customer and shows their number in the line
+  
+end
+
+def now_serving(deli)
+  if deli.length == 0 #checks if the line is empty
+    puts "There is nobody waiting to be served!"
+  else
+    name = deli.shift #assign the first person to the name variable and removes them from the line
+    puts "Currently serving #{name}." # wraps name variable for string interpolation to show the person being served
+  end
+end
+
+
+
+
+
+# Write your code here.
+=begin
 def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
   else
-    current_name = "The line is currently:"
-    katz_deli.each.with_index(1) do |person, i|
-      current_name << " #{i}. #{person}"
+    n = 1
+    names = ""
+    katz_deli.each do |x|
+      names += " #{n}. #{x}"
+      n += 1
     end
-    puts current_name
+    puts "The line is currently:#{names}"
   end
   
 end
@@ -27,3 +64,4 @@ def now_serving(katz_deli)
     puts "Currently serving #{name}."
   end
 end
+=end
