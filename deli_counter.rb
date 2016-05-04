33cf7e676c66c4bd047b98975cf33f396052057f
jjrajani <jjrajani@gmail.com>
2016-05-04 14:23:19 -0400

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,43 +1,32 @@
-#line = ["Logan", "Avi", "Spencer"]
 def line(line)
-  que = "The line is currently"
+  que = "The line is currently:"
   if line.size == 0
-    puts "#{que} empty."
+    que = que.chomp(que[que.size-1]) #to remove ':' from que
+    que += " empty."
   else
-  lineString = ''
     for i in 0...line.size
-      lineString += "#{i+1}. #{line[i]} "
+      que += " #{i+1}. #{line[i]}"
     end
-    lineString = lineString.chomp(lineString[lineString.size-1])
-    puts "#{que}: #{lineString}"
+  end
+  puts que
+end
+
+
+def take_a_number(line, name)
+  if line.size == 0
+    line.push(name)
+    puts "Welcome, #{name}. You are number #{line.size} in line."
+  else
+    line.push(name)
+    puts "Welcome, #{name}. You are number #{line.size} in line."
   end
 end
-#line(line)
-#line = ["Logan", "Avi", "Spencer"]
-#line = []
-#name = "jenna"
-def take_a_number(line, name)
-    line.push(name)
-    puts "Welcome, #{name}. You are number #{line.index(name)+1} in line."
-end
+
 
 def now_serving(line)
   if line.size == 0
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{line.shift}."
+    puts "Currently serving #{line.shift}." #to remove and print name at once
   end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
