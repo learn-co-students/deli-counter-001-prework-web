e9d189c5d564d16ef79a54cfc95d04bda66c9235
sourjam <jim@sidefra.me>
2016-05-05 21:52:16 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+
+def line(array)
+  if array.size == 0
+    puts "The line is currently empty."
+  else
+    string = "The line is currently:"
+    array.each do |name|
+      index = array.index(name) + 1
+      string.concat(" #{index}. #{name}")
+    end
+    puts string
+  end
+end
+
+def take_a_number(array, name)
+  array.push(name)
+  puts "Welcome, #{name}. You are number #{array.size} in line."
+  return array
+end
+
+def now_serving(array)
+  if array.size == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array.first}."
+    array.shift
+    return array
+  end
+end
