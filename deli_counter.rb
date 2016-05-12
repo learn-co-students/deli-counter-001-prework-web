3c6cb197ce277f851c2448caefadc19386179c92
ozPop <ozziepopovas@gmail.com>
2016-05-12 15:57:17 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,23 +1,25 @@
 # Write your code here.
-def line(arr)
-  if arr.size  == 0
-    puts "The line is currently empty."
+def line(deli_line)
+  output = ""
+  if deli_line.count == 0
+    output += "The line is currently empty."
   else
-    puts "The line is currently: "\
-      "#{arr.map.with_index { |name, i| "#{i+1}. "\
-      "#{name}"}.join(" ")}"
+    output += "The line is currently:"
+    deli_line.map.with_index{ |name, i| output += " #{i+1}. #{name}" }
   end
+  puts output
 end
 
-def take_a_number(arr, s)
-  arr.push(s)
-  puts "Welcome, #{s}. You are number #{arr.index(s)+1} in line."
+def take_a_number(deli_line, name)
+  deli_line << name
+  puts "Welcome, #{name}. You are number "\
+    "#{deli_line.find_index(name) + 1} in line."
 end
 
-def now_serving(arr)
-  if arr.empty?
+def now_serving(deli_line)
+  if deli_line.empty?
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{arr.shift}."
+    puts "Currently serving #{deli_line.shift}."
   end
 end
