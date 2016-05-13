ebe7a1c9e55838ac2d981cd73ada8951572b808d
jjrajani <jjrajani@gmail.com>
2016-05-13 17:10:04 -0400

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,6 +20,7 @@ def take_a_number(line, name)
     line.push(name)
     puts "Welcome, #{name}. You are number #{line.size} in line."
   end
+
 end
 
 
@@ -30,3 +31,44 @@ def now_serving(line)
     puts "Currently serving #{line.shift}." #to remove and print name at once
   end
 end
+
+
+
+
+
+
+
+roll = []
+
+def counter(roll, queue)
+  if roll.size === 0
+    ticket = 1
+    roll.push(ticket)
+    queue.push(ticket)
+  else
+    ticket = roll.size + 1
+    roll.push(ticket)
+    queue.push(ticket)
+  end
+  return ticket
+end
+
+def get_a_number(queue, roll)
+  ticket = counter(roll, queue)
+  return ticket
+end
+
+def serve_customer(queue)
+  queue = queue.shift()
+  return queue
+end
+
+#pull number
+#number push to queue [1]
+#pull
+#number push to queue [1,2]
+#serve
+#queue [2]
+#pull number
+#number push to queue [2,3]
+
