e3b8657df8ba1985bf820f483e97aa553114e495
Kevin Hernon <khernon1@gmail.com>
2016-05-18 18:31:57 -0400

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,12 @@
 def line(deli)
 str = ""
+i = 0
   if deli == []
     puts "The line is currently empty."
   else
     deli.each do |name|
-      i = deli.index(name)
-      str += " #{i+1}. " + name
+      i += 1
+      str += " #{i}. " + name
     end
     puts "The line is currently:#{str}"
   end
@@ -14,23 +15,34 @@ end
 def take_a_number(deli, person)
   if deli == []
     deli << person
-  deli.each do |name|
-    i = deli.index(name)
-    puts "Welcome, #{name}. You are number #{i+1} in line."
-  end
-else
-  deli << person
+    puts "Welcome, #{person}. You are number 1 in line."
+  else
+    deli << person
   puts "Welcome, #{person}. You are number #{deli.length} in line."
-end
+  end
 end
 
 def now_serving(deli)
   if deli == []
     puts "There is nobody waiting to be served!"
   else
-#    deli.each do |name|
-      puts "Currently serving #{deli[0]}."
-      deli.shift
-    end
+    puts "Currently serving #{deli[0]}."
+    deli.shift
+  end
+end
 
+def get_a_number(array)
+  if array == [] && @counter.to_i == 0
+    array << 1
+  elsif @counter.to_i != 0
+    array << @counter.to_i + 1
+  else
+    array << array.last + 1
+  end
+  array
+  @counter = array.last
+end
+
+def serve_customer(array)
+  array.shift
 end
