f52be7cb7608c8c45e5735beb6f7063e56a83d32
Green-stripes <kavericb@hotmail.com>
2016-05-19 21:14:05 +0100

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,45 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  arr = "The line is currently:"
+  if katz_deli != [] 
+  x = katz_deli.length 
+  count = 0
+  loop do
+    arr << " #{count+1}. #{katz_deli[count]}"
+    count += 1
+    break if count == x
+  end
+  puts "#{arr}"
+  else 
+    puts "The line is currently empty."
+  end
+end
+
+def take_a_number(katz_deli, person)
+  if katz_deli != []
+puts "Welcome, #{person}. You are number #{katz_deli.index(katz_deli.last) + 2} in line."
+else
+  puts "Welcome, #{person}. You are number 1 in line."
+end
+katz_deli << person
+end
+
+def now_serving(katz_deli)
+if katz_deli != []
+puts "Currently serving #{katz_deli.first}."
+else
+puts "There is nobody waiting to be served!"
+end
+katz_deli.shift
+end
+
+
+#def take_a_number(katz_deli, person)
+ # new_joiner = []
+  #person.each do |x| 
+  #new_joiner << puts "#{x} #{katz_deli.index(katz_deli.last)+1}"
+#end
+ # new_joiner
+#end
