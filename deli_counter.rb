1e1ce96b2877d81312c57e964fb7c17de90f120d
Corneezius <cfr2116@columbia.edu>
2016-05-26 15:31:24 -0400

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,10 @@
-katz_deli = []
-
-def line(katz_deli)
- if katz_deli.empty?
-   puts "The line is currently empty."
+def line(katz_deli) 
+ if katz_deli.empty? #use ruby method to check to see if the array has any values, returns true if it doesn't
+    puts "The line is currently empty."
  else
-    announcement = "The line is currently:"
-   other_deli.each_with_index do |value, index|
-    announcement += " #{index+1}. #{value}"
+    announcement = "The line is currently:" # set variable to start building announcement
+    katz_deli.each_with_index do |value, index| 
+    announcement += " #{index+1}. #{value}" #adds and assigns to announcement
    end
    puts announcement
  end
@@ -14,11 +12,11 @@ end
 
 def take_a_number(katz_deli, name)
   if katz_deli.empty?
-    puts "Welcome, #{name}. You are number #{katz_deli.size + 1} in line."
-    katz_deli << name
+     puts "Welcome, #{name}. You are number #{katz_deli.size + 1} in line." 
+     katz_deli << name #adds new customer to end of line
   else
    puts "Welcome, #{name}. You are number #{katz_deli.size + 1} in line."
-   katz_deli << name
+   katz_deli << name #adds new customer to end of line
   end
 end
 
@@ -26,7 +24,9 @@ def now_serving(katz_deli)
   if katz_deli.empty?
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{katz_deli.first}."
-   katz_deli.shift
+    puts "Currently serving #{katz_deli.first}." #show person at front of line
+    katz_deli.shift #show em the exit
   end
 end
+
+
