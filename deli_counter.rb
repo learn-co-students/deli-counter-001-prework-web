9eeb93d655b7eaa54632c7c72efdd96d7e6b5994
jinstrider2000 <efrainperezjr@live.com>
2016-06-04 21:43:26 -0400

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+
+katz_deli = []
+
+def line(customer_array)
+  if customer_array.empty?
+    puts "The line is currently empty."
+  else
+    message = "The line is currently:"
+    customer_array.each_with_index {|customer, position| message << " #{position+1}. #{customer}" }
+    puts message
+  end
+end
+
+def take_a_number(customer_array,new_customer)
+  customer_array << new_customer
+  puts "Welcome, #{new_customer}. You are number #{customer_array.length} in line."
+end
+
+def now_serving(customer_array)
+  customer_array.empty? ? puts("There is nobody waiting to be served!") : puts("Currently serving #{customer_array.shift}.")
+end
