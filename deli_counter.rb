079393c06423aab13cf34546fa00627c235a809d
Derick Quartey <niikwartey1@gmail.com>
2016-06-06 22:36:33 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,33 +1,34 @@
 # Write your code here.
-# #line takes a array of customers and prints their names and number in order
+
 def line(customers)
-  # initiate line_info prior to finding the info
-  line_info = ""
-
-  ## find out if there's actually a customer
-  # if not, line info should reflect that
-  # Otherwise, line info should list customer(s) with the position in line
-  if customers.size == 0
-    line_info = "The line is currently empty."
-  else
-    line_info = "The line is currently:"
-    customers.each {|c| line_info.concat(" #{customers.index(c) + 1}. #{c}")}
+  ###line ~ takes a array of customers and prints their names and number in order
+  if line.size == 0
+		line_info = "The line is currently empty."
+	else
+		line_info, num = "The line is currently:", 1
+    
+		line.each do |customer|
+			line_info << " #{num}. #{customer}"
+			num += 1
+	  end
   end
-  puts line_info
+
+	puts line_info
 end
 
-###ake_a_number adds a customer to the line and assigns them a number
-def take_a_number(customers, name)
-  customers.push(name)
-  puts "Welcome, #{name}. You are number #{customers.index(name) + 1} in line."
+
+def take_a_number(line, new_customer)
+  ###take_a_number ~ adds a customer to the line and assigns them a number
+	line << new_customer
+	puts "Welcome, #{new_customer}. You are number #{line.size} in line"
 end
 
-# #now_serving tells what customer is being served
-def now_serving(customers)
-  if customers.size == 0
-    puts "There is nobody waiting to be served!"
-  else
-    puts "Currently serving #{customers.first}."
-    customers.shift
-  end
+def now_serving(line)
+  ###now_serving tells what customer is being served
+	if line.size == 0
+		puts "There is nobody waiting to be served!"
+	else
+		current_customer = line.pop()
+		puts "Currently serving #{current_customer}."
+	end
 end
