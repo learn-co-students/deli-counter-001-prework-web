f13884c073a810d8ace44f5b1db37bed9780ad02
Natalisp <nathalipat@gmail.com>
2016-06-06 11:47:47 -0400

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,7 +22,7 @@ end
 def now_serving(katz_deli)
   if katz_deli.size == 0
     puts "There is nobody waiting to be served!"
-  elsif katz_deli.size != 0
+  else
     puts "Currently serving #{katz_deli.first}."
     katz_deli.shift
   end
