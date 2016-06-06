cb25a0867073859c7a58c7bce2a594ca870f6d30
Natalisp <nathalipat@gmail.com>
2016-06-06 11:35:05 -0400

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,6 @@
 # Write your code here.
 katz_deli = []
 
-def take_a_number(katz_deli, name)
-  katz_deli.push(name)
-    puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
-end
-
-
-
 def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
@@ -18,6 +11,12 @@ def line(katz_deli)
     end
     puts current_line.chop
   end
+end
+
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+    puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end
 
 def now_serving(katz_deli)
