92c764d39b9cb6c09babdeb6f43bec0683d32243
Bibi Khadiza <limakhadiza@yahoo.com>
2016-06-08 15:27:48 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    display = ""
+  display = ""
   katz_deli.each_with_index do |value, index|
     display += " #{index.to_i+1}. #{value}"
   end
