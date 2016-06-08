78da21bba803489906d5a116ab22ba25d1108dc2
Kelene <simoy93@gmail.com>
2016-06-07 20:29:12 -0400

    DONE

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,30 +1,32 @@
-katz_deli = [ ]
-#representing an empty array 
+katz_deli = []
 
-def line(katz_deli) #setting line method with the katz_deli argument 
-  if katz_deli.empty? #processes empty array
+def line(katz_deli)
+  if katz_deli.count == 0
     puts "The line is currently empty."
-  else 
-    puts "The line is currently: 1. #{katz_deli[0]} 2. #{katz_deli[1]} 3. #{katz_deli[2]}"
-    #this calls the array with its index and assigns them a number 
+  else
+    katz_deli != 0
+    answer = "The line is currently:"
+    katz_deli.each.with_index(1) do |index, customer|
+    answer += " #{customer}. #{index}"
+  end
+  puts "#{answer}"
   end
 end
 
 def take_a_number(katz_deli, customer)
-  if katz_deli.empty? #as there is no one in the line we call the empty array
-    katz_deli.push(customer) #this adds a customer to line
-    puts "Welcome, #{customer}. You are number 1 in line."
+ if katz_deli.count != 0
+  katz_deli << customer
+puts "Welcome, #{customer}. You are number #{katz_deli.count} in line."
   else 
-    katz_deli.push(customer) #this adds a customer to the end of the line
-    puts "Welcome, #{customer}. You are number 4 in line."
+    katz_deli << customer
+  puts "Welcome, #{customer}. You are number #{katz_deli.count} in line."
   end
 end
 
 def now_serving(katz_deli)
-  if katz_deli.empty? #calls the empty array 
-    puts "There is nobody waiting to be served!"
+  if katz_deli.count != 0
+    puts "Currently serving #{katz_deli.shift}."
   else
-    puts "Currently serving #{katz_deli[0]}."
-    katz_deli.shift #this removes customer from the line
+    puts "There is nobody waiting to be served!"
   end
 end
