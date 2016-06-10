1faaaf1b0125210d714c2ecfdb6b7fbcf83ed4d9
Parmanand Sahadeo <parmsahadeo530@gmail.com>
2016-06-10 22:38:10 +0000

    Modified

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+def line(array)
+  if (array.size == 0)
+    puts "The line is currently empty."
+  else
+    puts "The line is currently: "
+  end
+end
+
+def take_a_number(array, name)
+  puts "Welcome, #{name}. You are number #{array[]} in line."
+end
+
+def now_serving(array)
+  puts "Currently serving #{array[]}."
+end
