b451dde964d9966430e1af850cfb8852e9b5bd17
Hafizur Rahman <hafizur.rahmanc@gmail.com>
2016-06-11 21:17:55 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,32 @@
 # Write your code here.
+def line(deli)
+  if (deli.empty?)
+    puts "The line is currently empty."
+  else
+    count = 0
+    result = "The line is currently:"
+    while count < deli.size do
+      result = result.concat(" #{count + 1}. #{deli[count]}")
+      count += 1
+    end
+    puts result
+  end
+end
+
+def take_a_number(deli, name)
+  if (deli.empty?)
+    deli.push(name)
+    puts "Welcome, #{name}. You are number 1 in line."
+  else
+    deli.push(name)
+    puts "Welcome, #{name}. You are number #{deli.size} in line."
+  end
+end
+
+def now_serving(deli)
+  if (deli.empty?)
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{deli.shift}."
+  end
+end
