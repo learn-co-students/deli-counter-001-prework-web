4635031ce73de7616fa581f53f71e4a92497f1b6
Parmanand Sahadeo <parmsahadeo530@gmail.com>
2016-06-11 02:54:13 +0000

    Completed body for deli_counter.rb

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,16 +1,40 @@
 # Write your code here.
 def line(array)
-  if (array.size == 0)
+  size = array.size
+  temp_array = []
+  counter = 1
+  person = 0
+  if(size == 0)
     puts "The line is currently empty."
   else
-    puts "The line is currently: "
+    temp_array << "The line is currently:"
+    until(person == size)
+      temp_array << " #{counter}. #{array[person]}"
+      counter += 1
+      person += 1
+    end
+    updated_array = temp_array.join
+    puts updated_array
   end
 end
 
 def take_a_number(array, name)
-  puts "Welcome, #{name}. You are number #{array[]} in line."
+  size = array.size
+  if(size == 0)
+    array.push(name)
+    puts "Welcome, #{name}. You are number #{size+1} in line."
+  else
+    array.push(name)
+    puts "Welcome, #{name}. You are number #{size+1} in line."
+  end
 end
 
 def now_serving(array)
-  puts "Currently serving #{array[]}."
+  size = array.size
+  if(size == 0)
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array[0]}."
+    array.shift
+  end
 end
