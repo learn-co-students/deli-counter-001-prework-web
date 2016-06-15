8d51c1490e542f2678c6483bbb401383faf94ef5
Mohammed Omar <samiomarmohammed@gmail.com>
2016-06-15 20:19:15 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,16 @@
 # Write your code here.
+katz_deli = []
+
 def line(katz_deli)
   if katz_deli.count == 0
     puts "The line is currently empty."
   else katz_deli.count != 0
-    puts "The line is currently: 1. #{katz_deli[0]} 2. #{katz_deli[1]} 3. #{katz_deli[2]}"
+      string = "The line is currently:"
+      line_string = ""
+      katz_deli.each_with_index { |e,i| line_string = line_string + " #{i+1}. " + "#{e}"}
+      puts string + line_string
+    end
   end
-end
 
 def take_a_number(katz_deli, name)
   katz_deli << name
