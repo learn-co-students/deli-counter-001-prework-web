2dffee91406e9e1a7d3d658f2a774224fe1c9661
Hafizur Rahman <hafizur.rahmanc@gmail.com>
2016-06-15 19:44:28 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,7 +12,7 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
-    katz_deli.push(name)
+    katz_deli << name
     puts "Welcome, #{name}. You are number #{katz_deli.index(name) + 1} in line."
 end
 
