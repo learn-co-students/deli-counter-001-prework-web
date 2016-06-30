1c6cd85d9d0f11ec11126623b614708b1fdec729
Schyler Hosler <schylerhosler@gmail.com>
2016-06-30 17:18:06 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 # Write your code here.
-katz_deli = []
+#katz_deli = []
 
-def line(deli)
+def original_line(deli)
   if deli.empty?
     puts "The line is currently empty."
   else
@@ -10,7 +10,6 @@ def line(deli)
       deli_line << " #{(index+1)}. #{customer}"
     end
     puts deli_line
-    #puts "The line is currently: #{(deli[]+1)}. #{name}" ???
   end
 end
 
@@ -22,7 +21,7 @@ end
 #  end
 #end
 
-def take_a_number(deli, customer)
+def original_take_a_number(deli, customer)
   if deli.empty? && customer.empty?
     puts "There is nobody waiting to be served!"
   else
@@ -39,11 +38,59 @@ end
 #      expect(katz_deli).to eq(["Ada"])
 #    end
 
+def original_now_serving(deli)
+  if deli.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    customer = deli.shift
+    puts "Currently serving #{customer}."
+  end
+end
+
+
+
+def line(deli)
+  deli_line = "The line is currently:"
+  if deli.empty?
+    puts "The line is currently empty."
+  else
+    deli.each_with_index do |customer, index|
+      deli_line << " #{index+1}. #{customer}"
+      # or
+      #deli_line += " #{index + 1}. #{customer}"
+      # or
+      #deli_line = deli.map.with_index(1) do |e, i|
+        #"#{i}. #{e}"
+      #end
+      #deli_line.join(' ')
+      #puts "The line is currently: #{line}"
+    end
+    puts deli_line
+  end
+end
+
+def take_a_number(deli, customer)
+  if deli.empty? && customer.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    deli.push(customer)
+    puts "Welcome, #{customer}. You are number #{deli.index(customer)+1} in line."
+  end
+end
+
 def now_serving(deli)
   if deli.empty?
     puts "There is nobody waiting to be served!"
   else
     customer = deli.shift
     puts "Currently serving #{customer}."
+    # or
+    #puts "Currently serving #{deli[0]}."
+		#deli.shift
+    # or
+    #puts "Currently serving #{deli.first}."
+    #deli.shift
+    # or
+    #puts "Currently serving #{deli.shift}."
   end
 end
