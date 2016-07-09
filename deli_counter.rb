5174ae987e9c79f6416f594813303e4a00bb9b63
Jason Arnold <jason.arnold.00@gmail.com>
2016-07-09 21:23:46 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+katz_deli = []
+
+def line(arr)
+  if arr.count == 0
+    puts "The line is currently empty."
+  else
+    message = "The line is currently:"
+    arr.each_with_index do | value, index |
+      message += " #{index + 1}. #{value}"
+    end
+    puts message
+  end
+end
+
+def take_a_number(arr, str)
+  
+end
