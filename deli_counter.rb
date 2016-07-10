127ad5e6a39161b69244f886b0ad96d0229c07ea
Guangwen Xu <guangw.xu@gmail.com>
2016-07-10 19:21:47 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,26 +1,26 @@
 # Write your code here.
-def line(stJonhs_deli)
-  if stJonhs_deli.empty?
+def line(deli)
+  if deli.empty?
     puts "The line is currently empty."
-  else 
+  else
     currentLine = "The line is currently:"
-    stJonhs_deli.each_with_index.map do |customer, number|
-    currentLine << " #{number+1}. #{customer}"
+    deli.each_with_index do |name, index|
+      currentLine << " #{index+1}. #{name}"
     end
     puts currentLine
   end
 end
 
-def take_a_number(stJonhs_deli, customer)
-    stJonhs_deli << customer
-    puts "Welcome, #{customer}. You are number #{stJonhs_deli.length} in line."
+def take_a_number(deli, name)
+    deli << name
+    puts "Welcome, #{name}. You are number #{deli.length} in line."
 end
 
-def now_serving(stJonhs_deli)
-  if stJonhs_deli.empty?
+def now_serving(deli)
+  if deli.empty?
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{stJonhs_deli.first}."
-    stJonhs_deli.shift
+    puts "Currently serving #{deli[0]}."
+    deli.shift
   end
 end
