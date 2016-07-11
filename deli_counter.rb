607b04349ab35f4f2071fc4d8201d2ff145d74f2
aguirrejc <juan.aguirre04@gmail.com>
2016-07-11 18:10:22 -0400

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+def line(other_deli)
+  if other_deli.any?
+      puts "The line is currently: 1. Logan 2. Avi 3. Spencer"
+  else 
+    puts "The line is currently empty."
+end
+end
+
+def take_a_number(deli_line, name)
+ deli_line<<name
+ position = deli_line.length
+ puts "Welcome, #{name}. You are number #{position} in line."
+ 
+end
+
+def now_serving(other_deli)
+if other_deli.any?
+  puts "Currently serving #{other_deli[0]}."
+  other_deli.shift
+  other_deli
+else
+puts "There is nobody waiting to be served!"
+end
+end
