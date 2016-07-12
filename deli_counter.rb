17ee4847f2ed9a820cb8aeae44b00b6bbcf8a808
Keo  Bun <keonilrath@gmail.com>
2016-07-12 23:28:17 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
-  elsif katz_deli.length > 0
+  else
     line = "The line is currently:"
     katz_deli.each_with_index do |customer, number|
       line << " #{number + 1}. #{customer}"
