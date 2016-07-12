83539fe4d464d85faa7535cf8f5ba24f69017ecb
Guangwen Xu <guangw.xu@gmail.com>
2016-07-12 03:24:56 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,6 @@
-# Write your code here.
+# Shows everyone their current place in the line. If there is nobody in line, it outputs "The line is currently empty.".
+# Params:
+# +deli+:: +Array+ the collection of current customers' names at the deli.
 def line(deli)
   if deli.empty?
     puts "The line is currently empty."
@@ -11,11 +13,18 @@ def line(deli)
   end
 end
 
+# Adds a person to the line at the deli, then returns the person's name along with their position in line.
+# Params:
+# +deli+:: +Array+ the collection of current customers' names at the del.
+# +name+:: +String+ The name of the person to add to the deli line.
 def take_a_number(deli, name)
     deli << name
     puts "Welcome, #{name}. You are number #{deli.length} in line."
 end
 
+# Output the next person in line and then removes them from the front. If there is nobody in line, it outputs "There is nobody waiting to be served!".
+# Params:
+# +deli+:: +Array+ the collection of current customers' names at the del.
 def now_serving(deli)
   if deli.empty?
     puts "There is nobody waiting to be served!"
