e3ab00962ba53720af690129d9f5d655934939bc
dimitriwashere <dimitriwashere@gmail.com>
2016-07-16 21:40:25 -0400

    Update deli_counter.rb

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ def line(katz_deli)
   else
     string = "The line is currently:"
     katz_deli.each_with_index do |name, index|
-      string << (" #{index + 1}. #{name}")
+      string << " #{index + 1}. #{name}"
     end
     puts string
   end
