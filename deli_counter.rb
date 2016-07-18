0ef2063837db1c400967df9a400abf26f6521cde
arb1nsnmgl <arvinsanmiguel@gmail.com>
2016-07-18 19:14:51 -0400

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,50 @@
 # Write your code here.
+
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.empty? == true
+    puts "The line is currently empty."
+  elsif katz_deli.empty? == false
+    other_deli = Array.new
+    for name in katz_deli
+      idx = katz_deli.index(name) + 1
+      position = "#{idx}. #{name}"
+      other_deli.push(position)
+    end
+    puts "The line is currently: #{other_deli.join(" ")}"
+  end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli.push("#{name}")
+  index = katz_deli.index(name)
+  index += 1
+  puts "Welcome, #{name}. You are number #{index} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli.empty? == false
+    puts "Currently serving #{katz_deli.first}."
+    katz_deli.shift
+  else
+    puts "There is nobody waiting to be served!"
+  end
+end
+
+
+# name = ["arvin", "shiela", "may", "sanmiguel", "lachica"]
+# katz_deli = []
+# for x in name 
+#   idx = name.index(x) + 1
+#   new_name = "#{idx}. #{x}"
+#   katz_deli.push(new_name)
+# end
+
+
+
+
+
+
+
+
