d64adaaba5a521f19cb7d30a9284d074b725b62d
hkim53 <paragon.hankido@gmail.com>
2016-07-27 12:57:34 -0400

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 katz_deli = []
 
 def line (katz_deli)
-  if katz_deli.count > 1
+  if katz_deli.count > 0
     output = "The line is currently:"
     katz_deli.each_with_index do |name, index|
       output << " #{index +1}. #{name}"
