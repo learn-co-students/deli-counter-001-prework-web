b38dde7f70189e75869ea6dcb1d08056d33f37db
slindquist3 <slindquist3@gmail.com>
2016-08-01 11:16:26 -0400

    Added to Deli-Counter

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,28 +1,28 @@
 def line(katz_deli)
-  line_up = "The line is currently"
+  list = "The line is currently"
   if katz_deli.empty? == true
-    puts line_up.concat " empty."
+    list << " empty."
   else
-    line_up = line_up + ":"
+    list << ":"
     katz_deli.each_with_index do |name, index|
-      line_up.concat(" #{index + 1}. #{name}")
+      list << " #{index + 1}. #{name}"
     end
-    puts line_up
   end
+  puts list
 end
 
-def take_a_number(katz_deli, string)
-  katz_deli = katz_deli.push(string)
-  last_person = katz_deli.last
-  last_place = katz_deli.count
-  puts "Welcome, #{last_person}. You are number #{last_place} in line."
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.count} in line."
 end
 
 def now_serving(katz_deli)
-  if katz_deli.empty? == true
+  next_name = katz_deli[0]
+
+  if next_name == nil
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving " + katz_deli[0] + "."
-    katz_deli.shift
+    puts "Currently serving #{next_name}."
   end
+  katz_deli.shift
 end
