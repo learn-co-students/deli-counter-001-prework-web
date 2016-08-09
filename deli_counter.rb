0f210326fb49e012fc53b8840346423ca9826837
Rebecca Yi <rebeccayeyi@gmail.com>
2016-08-09 15:55:49 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,20 +1,19 @@
-def line(arr)
-  line = []
-  if arr.size == 0
+def line (katz_deli)
+  new_line = []
+  if katz_deli.size == 0
     puts "The line is currently empty."
-  else
-    arr.each.with_index do |n,i|
-      line << "#{i+1}. #{n}"
-    end
-    puts "The line is currently: #{line.join(" ")}"
+  else katz_deli.each_with_index do |name, i|
+      new_line << "#{i+1}. #{name}"
+        end
+    puts "The line is currently: #{new_line.join(" ")}"
   end
 end
 
-def take_a_number(arr,name)
-  arr << name
-  puts "Welcome, #{name}. You are number #{arr.length} in line."
+def take_a_number (katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
-def now_serving(arr)
-  puts arr.size == 0 ? "There is nobody waiting to be served!": "Currently serving #{arr.shift}."
+def now_serving (katz_deli)
+  puts katz_deli.size == 0? "There is nobody waiting to be served!" : "Currently serving #{katz_deli.shift}."
 end
