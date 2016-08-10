46fadf8f2461508b00125082322b493d6e74ee76
Rebecca Yi <rebeccayeyi@gmail.com>
2016-08-10 14:52:06 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,16 +2,15 @@ def line (katz_deli)
   new_line = []
   if katz_deli.size == 0
     puts "The line is currently empty."
-  else katz_deli.each_with_index do |name, i|
-      new_line << "#{i+1}. #{name}"
-        end
+  else
+    katz_deli.each_with_index {|name, i| new_line << "#{i + 1}. #{name}"}
     puts "The line is currently: #{new_line.join(" ")}"
   end
 end
 
 def take_a_number (katz_deli, name)
   katz_deli << name
-  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end
 
 def now_serving (katz_deli)
