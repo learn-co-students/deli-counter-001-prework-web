63e3a7c6f4ce1b3f862e7525706abfa2338abc50
Jason Kwong <jasonkwong11@gmail.com>
2017-09-03 19:47:26 -0700

    refactor

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,41 +1,29 @@
-katz_deli = []
-
 def line(katz_deli)
+  phrase = "The line is currently: "
 
-if katz_deli == []
-  puts "The line is currently empty."
-else
-  message_parttwo = []
-  message_partone = "The line is currently: "
-  katz_deli.each_with_index do |name, index|
-  place_in_line = index + 1
-    message_each_person = "#{place_in_line}. #{name} "
-    message_parttwo.push(message_each_person)
+  if katz_deli.empty?
+    puts "The line is currently empty."
+    return
   end
 
-output = message_partone << message_parttwo = message_parttwo.join[0...-1]
-  puts "#{output}"
-end
+  katz_deli.each_with_index do |person, i|
+    queue_num = (i + 1).to_s
+    phrase << "#{queue_num}. #{person} "
+  end
+  puts phrase[0..phrase.length-2]
 end
 
 def take_a_number(katz_deli, person)
-katz_deli.push(person)
-number_in_line = katz_deli.index(person) + 1
-puts "Welcome, #{person}. You are number #{number_in_line} in line."
+  katz_deli.push(person)
+  puts "Welcome, #{person}. You are number #{katz_deli.index(person) + 1} in line."
 end
 
 def now_serving(katz_deli)
-if katz_deli == []
-  puts "There is nobody waiting to be served!"
-else
-##find the method that lets you retrive the object, given the index. index (0), puts "serving that person" and the remove that object from the array"
-first_person = katz_deli[0]
-  puts "Currently serving #{first_person}."
-  katz_deli.shift
+  if katz_deli.empty?
+    puts "There is nobody waiting to be served!"
+    return
+  end
+
+  person = katz_deli.shift
+  puts "Currently serving #{person}."
 end
-end
-
-
-
-
-
